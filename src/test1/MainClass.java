a5a3034ce64d62ba10421541b653b2ec7f5a2a9e
Konstantin <konstantin.selyuk@yahoo.com>
2015-04-16 23:28:36 +0300

    Example for solving first task

--- src/test1/MainClass.java
+++ src/test1/MainClass.java
@@ -4,7 +4,23 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		MainClass mc = new MainClass();
+		// Викличемо функцію, і перевіримо правильність результату
+		double x = 23.4;
+		double y = mc.function(23.4);
+		System.out.println("x = " + x + "; and y = " + y);
 
+	}
+	
+	public double function(double x) {
+		double y = 0;
+		// Firstly check correctness of input x
+		if(Math.cos(x * x) >= 0) {
+			y = Math.sqrt(Math.cos(x * x));
+		} else {
+			System.out.println("Incorrect input value x!");
+		}
+		return y;
 	}
 
 }
